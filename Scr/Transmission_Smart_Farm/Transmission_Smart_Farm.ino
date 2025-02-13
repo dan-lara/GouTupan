2300d452a2d195291547b1ca0ebc2d03c8d92866
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2025-02-14 00:49:04 +0100

    TEST firmware version - Closes #29

--- Scr/Transmission_Smart_Farm/Transmission_Smart_Farm.ino
+++ Scr/Transmission_Smart_Farm/Transmission_Smart_Farm.ino
@@ -1,14 +1,27 @@
-//TEST carte fonctionnelle 
+// Test pour obtenir firmware Version du module : ARD-078 1.2.3
+
+#include <MKRWAN.h>
+
+LoRaModem modem;
 
 void setup() 
 {
-  pinMode(6, OUTPUT); // Définit la LED intégrée comme sortie
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!modem.begin(EU868)) 
+  {  // Choisir la bande régionale correcte (EU868 ici)
+    Serial.println("Échec de démarrage du module");
+    while (1);
+  }
+
+  // Affiche la version du firmware du module LoRa
+  Serial.print("Version du module : ");
+  Serial.println(modem.version());  // Affiche la version du firmware
+
 }
 
 void loop() 
 {
-  digitalWrite(6, HIGH); // Allume la LED
-  delay(1000); // Attendre 1 seconde
-  digitalWrite(6, LOW); // Éteint la LED
-  delay(1000); // Attendre 1 seconde
+  // NOP
 }
