56161c0b9fb365e5313a3faa159b880e243edeca
5b25 <FMT200907@outlook.com>
2025-04-06 21:32:18 +0200

    Added images used in Wiki

---
+++ Firmware/epd2in9b_V3/font16.hpp
@@ -0,0 +1,10 @@
+#ifndef FONT16_H
+#define FONT16_H
+
+#include "fonts.h"  // 复用 fonts.h 中的 sFONT 结构
+
+// 声明 font16.cpp 中定义的全局变量
+extern const uint8_t Font16_Table[] PROGMEM;
+extern sFONT Font16;
+
+#endif
